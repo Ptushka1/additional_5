833f8aa79cdf3a86afea266c7f03477e2b81c03c
Elena <ptushka1@rambler.ru>
2018-02-28 22:00:35 +0300

    Update index.js
    solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var i,
+      symbol,
+      temporarySymbol;
+  var lengthStr = str.length;
+
+  var temp = [],
+      openingBrackets = [],
+      closingBrackets = [];
+
+  bracketsConfig.map(function (element) {
+    openingBrackets.push(element[0]);
+    closingBrackets.push(element[1]);
+  });
+
+  temp.push(str[0]);
+
+  for (i = 1; i < lengthStr; i++) {
+    symbol = str[i];
+
+    if (openingBrackets.indexOf(symbol) > -1) {
+      if (openingBrackets.indexOf(symbol) > -1 && closingBrackets.indexOf(symbol) > -1) {
+        if (symbol == temp[temp.length - 1]) {
+          temp.pop();
+          continue;
+        }
+      }
+
+      temp.push(symbol);
+    } else if (closingBrackets.indexOf(symbol) > -1) {
+
+      temporarySymbol = openingBrackets[closingBrackets.indexOf(symbol)];
+      if (temp.length === 0 || temp.pop() !== temporarySymbol) {
+        return false;
+      }
+    }
+  }
+
+  if (temp.length !== 0) {
+    return false;
+  }
+
+  return true;
+}
 }
